ff9c0d2476dbbfa8a79ff1c6aec1a5add87da136
desmond276 <DLAU@Desmonds-MacBook-Pro.local>
2018-04-29 16:35:39 -0700

    jquery and document.ready

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,30 +1,33 @@
+$(function() {
 // alert('hi');
 
-// Test shit from interact.js
-var element = document.getElementById('grid-snap'),
-    x = 0, y = 0;
+	// Test shit from interact.js
+	var element = document.getElementById('grid-snap'),
+	    x = 0, y = 0;
 
-interact(element)
-  .draggable({
-    snap: {
-      targets: [
-        interact.createSnapGrid({ x: 30, y: 30 })
-      ],
-      range: Infinity,
-      relativePoints: [ { x: 0, y: 0 } ]
-    },
-    inertia: true,
-    restrict: {
-      restriction: element.parentNode,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-      endOnly: true
-    }
-  })
-  .on('dragmove', function (event) {
-    x += event.dx;
-    y += event.dy;
+	interact(element)
+	  .draggable({
+	    snap: {
+	      targets: [
+	        interact.createSnapGrid({ x: 30, y: 30 })
+	      ],
+	      range: Infinity,
+	      relativePoints: [ { x: 0, y: 0 } ]
+	    },
+	    inertia: true,
+	    restrict: {
+	      restriction: element.parentNode,
+	      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+	      endOnly: true
+	    }
+	  })
+	  .on('dragmove', function (event) {
+	    x += event.dx;
+	    y += event.dy;
 
-    event.target.style.webkitTransform =
-    event.target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
-  });
+	    event.target.style.webkitTransform =
+	    event.target.style.transform =
+	        'translate(' + x + 'px, ' + y + 'px)';
+	  });
+});
+
