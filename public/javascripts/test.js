03f8b7c4608e504e49a1336eb2bf645de02864f3
desmond276 <DLAU@Desmonds-MacBook-Pro.local>
2018-05-01 18:50:17 -0700

    fix drag issue with multiple elements starting from previous element

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -7,30 +7,45 @@ $(function() {
 
 	var element = document.getElementsByClassName('.draggable'),
 	    x = 0, y = 0;
+	    console.log(element);
 
 	interact('.draggable')
-	  .draggable({
-	    snap: {
-	      targets: [
-	        interact.createSnapGrid({ x: 30, y: 30 })
-	      ],
-	      range: Infinity,
-	      relativePoints: [ { x: 0, y: 0 } ]
-	    },
-	    restrict: {
-	      restriction: element.parentNode,
-	      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-	      endOnly: true
-	    }
-	  })
-	  .on('dragmove', function (event) {
-	    x += event.dx;
-	    y += event.dy;
+		.draggable({
+			snap: {
+		  		targets: [
+		    		interact.createSnapGrid({ x: 30, y: 30 })
+				],
+			  	range: Infinity,
+				relativePoints: [ { x: 0, y: 0 } ]
+			},
+			restrict: {
+		  		restriction: element.parentNode,
+	  			elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+		  		endOnly: true
+			}
+		})
+		.on('dragmove', function (event) {
+			var target = event.target,
+	        // keep the dragged position in the data-x/data-y attributes
+	        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+	        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-	    event.target.style.webkitTransform =
-	    event.target.style.transform =
-	        'translate(' + x + 'px, ' + y + 'px)';
-	  });
+	    // translate the element
+	    target.style.webkitTransform =
+	    target.style.transform =
+	      'translate(' + x + 'px, ' + y + 'px)';
+
+	    // update the posiion attributes
+	    target.setAttribute('data-x', x);
+	    target.setAttribute('data-y', y);
+			// console.log(event);
+			// x += event.dx;
+			// y += event.dy;
+
+			// event.target.style.webkitTransform =
+			// event.target.style.transform =
+			//     'translate(' + x + 'px, ' + y + 'px)';
+		});
 
 	/**
 		param: id (string)
