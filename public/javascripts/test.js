da0c8c44ccd2f22f24fe25446ad4565a138c54d3
desmond <dlau@DLAUs-MacBook.local>
2018-05-11 18:20:13 -0700

    dropzone center of ele

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -11,49 +11,61 @@ $(function() {
 	interact('.draggable')
 		.draggable({
 			snap: {
-				mode: 'anchor',
-				anchors: [],
-				range: Infinity,
-				elementOrigin: { x: 0.5, y: 0.5 },
+				// mode: 'anchor',
+				// anchors: [],
+				// range: Infinity,
+				// elementOrigin: { x: 0.5, y: 0.5 },
+				relativePoints: [{x: 0.5, y:0.5}],
 				endOnly: true
 			},
 			restrict: {
 		  		restriction: 'parent',
 	  			elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
 		  		endOnly: true
-			}
+			},
+			inertia: false
 		})
 		.on('dragmove', function (event) {
+			// console.log(event);
 			var target = event.target,
 	        // keep the dragged position in the data-x/data-y attributes
 	        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
 	        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-	    // translate the element
-	    target.style.webkitTransform =
-	    target.style.transform =
-	      'translate(' + x + 'px, ' + y + 'px)';
 
-	    // update the posiion attributes
-	    target.setAttribute('data-x', x);
-	    target.setAttribute('data-y', y);
+		    // translate the element
+		    target.style.webkitTransform =
+		    target.style.transform =
+		      'translate(' + x + 'px, ' + y + 'px)';
+
+
+		    // update the posiion attributes
+		    target.setAttribute('data-x', x);
+		    target.setAttribute('data-y', y);
 		});
 
 	interact('.dropzone')
 		.dropzone({
-			overlap: 0.2,
+			overlap: 0.1	,
 			ondragenter: function(event){
+				// console.log(event);
+
+
 				var dropRect = interact.getElementRect(event.target),
 		        dropCenter = {
 		          x: dropRect.left + dropRect.width  / 2,
 		          y: dropRect.top  + dropRect.height / 2
 		        };
 
+		        // dropCenter.x = 400
+		        ;
 			    event.draggable.draggable({
 			    	snap: {targets: [dropCenter]}
 			    });
+			    console.log(dropRect);
 			    console.log(dropCenter);
-				console.log(event.draggable.draggable());
+
+				// console.log(event.draggable.draggable(	));
 			},
 			ondragleave: function(event){
 				event.draggable.draggable({
@@ -61,11 +73,6 @@ $(function() {
 				});
 			}
 		});
-
-//   .on('dragleave', function (event) {
-    // event.draggable.snap(false);
-  // });
-
 
 	/**
 		param: id (string)
