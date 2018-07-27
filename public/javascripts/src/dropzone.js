8e501e2299f926bd5d35e93959e8bf09662ae35a
Zackariah <zackariah.gauntt@gmail.com>
2018-07-26 21:53:39 -0700

    More shit, but it's broke

--- public/javascripts/src/dropzone.js
+++ public/javascripts/src/dropzone.js
@@ -11,14 +11,6 @@ function gridContract(xCoord,yCoord){
 	$('.dropzone[data-xCoordDZ=' + xCoord + '][data-yCoordDZ='+ yCoord +']').remove();
 }
 
-//Container sizing function
-function sizeObjCont(xCoord,yCoord){
-	var contWidthPx = $('.dropzone.first').width() * (xCoord+1);
-	var contHeightPx = $('.dropzone.first').height() * (yCoord+1);
-	$("#object_container").css({width: contWidthPx, height: contHeightPx});
-	$("#object_container_overlay").css({width: contWidthPx, height: contHeightPx});//Temp; need to get rid of the overlay, but some dependency is causing issues.
-}
-
 //Grid initialization	
 var initXCoord = 0;
 var initYCoord = 0;
@@ -33,10 +25,26 @@ var yDZCoord = initRowSize; //Keeps track of dropzone grid size in the y directi
 //Dropzone positioning array
 var dzPosArr = new Array(initColSize+1);
 
+var gridInitCoord = {
+	//grid init
+	initXCoord: 0,
+	initYCoord: 0,
+	// set init grid size
+	initRowSize: 3,
+	initColSize: 11
+}
 
-sizeObjCont(initColSize,initRowSize);
+
+// sizeObjCont(initColSize,initRowSize);
 
 module.exports = {
+	//Container sizing function
+	sizeObjCont: function(xCoord,yCoord){
+		var contWidthPx = $('.dropzone.first').width() * (xCoord+1);
+		var contHeightPx = $('.dropzone.first').height() * (yCoord+1);
+		$("#object_container").css({width: contWidthPx, height: contHeightPx});
+		$("#object_container_overlay").css({width: contWidthPx, height: contHeightPx});//Temp; need to get rid of the overlay, but some dependency is causing issues.
+	},
 	//First for loop expands rows
 	startGrid: function(){
 	for(initYCoord=0; initYCoord <= initRowSize; initYCoord++) {
@@ -140,6 +148,10 @@ module.exports = {
 
 			$('.selected').remove();
 		});
+	},
+	//get the inital coords so we can size the obj container first.
+	getGridCoordinates: function(){
+		return gridInitCoord;
 	}
 
 
--- public/javascripts/src/main.js
+++ public/javascripts/src/main.js
@@ -5,6 +5,8 @@ var dropzone = require('./dropzone.js')
 $(function(){
 	// test.getDropDimensions();
 	test.init();
+	// console.log(dropzone.getGridCoordinates().initRowSize);
+	dropzone.sizeObjCont(dropzone.getGridCoordinates().initColSize,dropzone.getGridCoordinates().initRowSize);//THIS SHIT AIN'T WERKIN'
 	dropzone.startGrid();
 	dropzone.initDzArr();
 	dropzone.addXDZ();
