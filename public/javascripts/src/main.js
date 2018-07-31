7c888b8cd6c3f4dee1500fbb24484bea07de6d40
Zackariah <zackariah.gauntt@gmail.com>
2018-07-30 21:13:26 -0700

    made more descriptive variable names

--- public/javascripts/src/main.js
+++ public/javascripts/src/main.js
@@ -1,21 +1,10 @@
-// replacement for ./public/javascripts/test.js
-var test = require('./init.js');
-
+var initial = require('./init.js');
 var dropzone = require('./dropzone.js')
 var element = require('./element.js');
 
-
 $(function(){
-	// test.getDropDimensions();
-	test.init();
-	var dropzoneDims = test.getDropDimensions();
-	console.log(dropzoneDims.dropHeight);
-	// console.log(dropzone.getGridCoordinates().initRowSize);
-	// dropzone.sizeObjCont(dropzone.getGridCoordinates().initColSize,dropzone.getGridCoordinates().initRowSize);//THIS SHIT AIN'T WERKIN'
-	// dropzone.sizeObjCont(11,3);
-	// console.log(dropzone.getGridCoordinates().initColSize);
-	// console.log(dropzone.getGridCoordinates().initRowSize);
-	// console.log(dropzone.sizeObjCont());
+	initial.init();
+	var dropzoneDims = initial.getDropDimensions();
 	dropzone.startGrid();
 	dropzone.initDzArr();
 	dropzone.addXDZ();
@@ -24,6 +13,6 @@ $(function(){
 	dropzone.deleteYDZ();
 	dropzone.deleteEls();
 
-	element.init(dropzoneDims.dropHeight, dropzoneDims.dropWidth, test.getElArr());
+	element.init(dropzoneDims.dropHeight, dropzoneDims.dropWidth, initial.getElArr());
 
 })
