416a9f4973ef7e941b6f928f06dc25476b5f01ab
Zackariah <zackariah.gauntt@gmail.com>
2018-07-30 21:04:54 -0700

    fixed the sizeObjCont bug

--- public/javascripts/src/main.js
+++ public/javascripts/src/main.js
@@ -1,12 +1,16 @@
 // replacement for ./public/javascripts/test.js
 var test = require('./init.js');
-var dropzone = require('./dropzone.js')
+var dropzone = require('./dropzone.js');
 
 $(function(){
 	// test.getDropDimensions();
 	test.init();
 	// console.log(dropzone.getGridCoordinates().initRowSize);
-	dropzone.sizeObjCont(dropzone.getGridCoordinates().initColSize,dropzone.getGridCoordinates().initRowSize);//THIS SHIT AIN'T WERKIN'
+	// dropzone.sizeObjCont(dropzone.getGridCoordinates().initColSize,dropzone.getGridCoordinates().initRowSize);//THIS SHIT AIN'T WERKIN'
+	// dropzone.sizeObjCont(11,3);
+	// console.log(dropzone.getGridCoordinates().initColSize);
+	// console.log(dropzone.getGridCoordinates().initRowSize);
+	// console.log(dropzone.sizeObjCont());
 	dropzone.startGrid();
 	dropzone.initDzArr();
 	dropzone.addXDZ();
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -557,136 +557,136 @@ $(function() {
 		return $('#' + elName).height();
 	}
 
-//Grid expansion function
-function gridExpand(xCoord,yCoord){
-	$("#object_container").append('<div class="dropzone first" data-xCoordDZ=' + xCoord + ' data-yCoordDZ='+ yCoord +'>');
-	$('.dropzone[data-xCoordDZ=' + xCoord + '][data-yCoordDZ='+ yCoord +']').css({left: $('.dropzone.first').width() * xCoord, top: $('.dropzone.first').height() * yCoord, position:'absolute'});
-}
+// //Grid expansion function
+// function gridExpand(xCoord,yCoord){
+// 	$("#object_container").append('<div class="dropzone first" data-xCoordDZ=' + xCoord + ' data-yCoordDZ='+ yCoord +'>');
+// 	$('.dropzone[data-xCoordDZ=' + xCoord + '][data-yCoordDZ='+ yCoord +']').css({left: $('.dropzone.first').width() * xCoord, top: $('.dropzone.first').height() * yCoord, position:'absolute'});
+// }
 
-//Grid contraction function
-function gridContract(xCoord,yCoord){
-	$('.dropzone[data-xCoordDZ=' + xCoord + '][data-yCoordDZ='+ yCoord +']').remove();
-}
+// //Grid contraction function
+// function gridContract(xCoord,yCoord){
+// 	$('.dropzone[data-xCoordDZ=' + xCoord + '][data-yCoordDZ='+ yCoord +']').remove();
+// }
 
-//Container sizing function
-function sizeObjCont(xCoord,yCoord){
-	var contWidthPx = $('.dropzone.first').width() * (xCoord+1);
-	var contHeightPx = $('.dropzone.first').height() * (yCoord+1);
-	$("#object_container").css({width: contWidthPx, height: contHeightPx});
-	$("#object_container_overlay").css({width: contWidthPx, height: contHeightPx});//Temp; need to get rid of the overlay, but some dependency is causing issues.
-}
+// //Container sizing function
+// function sizeObjCont(xCoord,yCoord){
+// 	var contWidthPx = $('.dropzone.first').width() * (xCoord+1);
+// 	var contHeightPx = $('.dropzone.first').height() * (yCoord+1);
+// 	$("#object_container").css({width: contWidthPx, height: contHeightPx});
+// 	$("#object_container_overlay").css({width: contWidthPx, height: contHeightPx});//Temp; need to get rid of the overlay, but some dependency is causing issues.
+// }
 
-//Grid initialization	
-	var initXCoord = 0;
-	var initYCoord = 0;
-	//Set initial grid size
-	var initRowSize = 3;
-	var initColSize = 11;
+// //Grid initialization	
+// 	var initXCoord = 0;
+// 	var initYCoord = 0;
+// 	//Set initial grid size
+// 	var initRowSize = 3;
+// 	var initColSize = 11;
 
-	//First for loop expands rows
-	for(initYCoord=0; initYCoord <= initRowSize; initYCoord++) {
-		gridExpand(0,initYCoord);
-			//Second for loop expands columns
-			for(initXCoord=1; initXCoord <= initColSize; initXCoord++) {
-				gridExpand(initXCoord,initYCoord);
-			};
-	};
+// 	//First for loop expands rows
+// 	for(initYCoord=0; initYCoord <= initRowSize; initYCoord++) {
+// 		gridExpand(0,initYCoord);
+// 			//Second for loop expands columns
+// 			for(initXCoord=1; initXCoord <= initColSize; initXCoord++) {
+// 				gridExpand(initXCoord,initYCoord);
+// 			};
+// 	};
 
-	sizeObjCont(initColSize,initRowSize);
+// 	sizeObjCont(initColSize,initRowSize);
 
-	//Initialize 2D array
-	var dzPosArr = new Array(initColSize+1);
+// 	//Initialize 2D array
+// 	var dzPosArr = new Array(initColSize+1);
 
-	for (i=0; i < (initColSize+1); i++) {
-		dzPosArr[i]=new Array(initRowSize+1);
-	};
+// 	for (i=0; i < (initColSize+1); i++) {
+// 		dzPosArr[i]=new Array(initRowSize+1);
+// 	};
 
-var xDZCoord = initColSize; //Keeps track of dropzone grid size in the x direction.
-var yDZCoord = initRowSize; //Keeps track of dropzone grid size in the y direction.
+// var xDZCoord = initColSize; //Keeps track of dropzone grid size in the x direction.
+// var yDZCoord = initRowSize; //Keeps track of dropzone grid size in the y direction.
 
-//Button function for adding dropzone columns (later this will be triggered by elements nearing the edge, as well as button)
-$('.button_horizontal').click(function () {
-	  	xDZCoord++
-	  	//For everything after new rows have been added
-	  	if (yDZCoord != 0) {
-			var tempCount = 0;
-			for(tempCount=0; tempCount <= yDZCoord; tempCount++) {
-				gridExpand(xDZCoord,tempCount);
-			};
-		} else {
-			//For before new rows have been added
-			gridExpand(xDZCoord,yDZCoord);
-		}
-		//Increase array in the x direction with same size rows in the new x slot
-		dzPosArr.length = xDZCoord+1;
-		for (i=xDZCoord; i < (xDZCoord+1); i++) {
-			dzPosArr[i]=new Array(yDZCoord+1);
-		};
+// //Button function for adding dropzone columns (later this will be triggered by elements nearing the edge, as well as button)
+// $('.button_horizontal').click(function () {
+// 	  	xDZCoord++
+// 	  	//For everything after new rows have been added
+// 	  	if (yDZCoord != 0) {
+// 			var tempCount = 0;
+// 			for(tempCount=0; tempCount <= yDZCoord; tempCount++) {
+// 				gridExpand(xDZCoord,tempCount);
+// 			};
+// 		} else {
+// 			//For before new rows have been added
+// 			gridExpand(xDZCoord,yDZCoord);
+// 		}
+// 		//Increase array in the x direction with same size rows in the new x slot
+// 		dzPosArr.length = xDZCoord+1;
+// 		for (i=xDZCoord; i < (xDZCoord+1); i++) {
+// 			dzPosArr[i]=new Array(yDZCoord+1);
+// 		};
 
-		sizeObjCont(xDZCoord,yDZCoord);
-});
+// 		sizeObjCont(xDZCoord,yDZCoord);
+// });
 
-//Button function for adding dropzone rows (later this will be triggered by elements nearing the edge, as well as button)
-$('.button_vertical').click(function () {
-		yDZCoord++
-		//For everything after new columns have been added
-	  	if (xDZCoord != 0) {
-			var tempCount = 0;
-			for(tempCount=0; tempCount <= xDZCoord; tempCount++) {
-				gridExpand(tempCount,yDZCoord);
-			};
-		} else {
-			//For before new columns have been added
-			gridExpand(xDZCoord,yDZCoord);
-		}
-		//Increase array in the y direction
-		for (i=0; i < (xDZCoord+1); i++) {
-			dzPosArr[i].length++;
-		};
+// //Button function for adding dropzone rows (later this will be triggered by elements nearing the edge, as well as button)
+// $('.button_vertical').click(function () {
+// 		yDZCoord++
+// 		//For everything after new columns have been added
+// 	  	if (xDZCoord != 0) {
+// 			var tempCount = 0;
+// 			for(tempCount=0; tempCount <= xDZCoord; tempCount++) {
+// 				gridExpand(tempCount,yDZCoord);
+// 			};
+// 		} else {
+// 			//For before new columns have been added
+// 			gridExpand(xDZCoord,yDZCoord);
+// 		}
+// 		//Increase array in the y direction
+// 		for (i=0; i < (xDZCoord+1); i++) {
+// 			dzPosArr[i].length++;
+// 		};
 
-		sizeObjCont(xDZCoord,yDZCoord);
-});
+// 		sizeObjCont(xDZCoord,yDZCoord);
+// });
 
-//Button function for deleting dropzone columns (later this will be triggered by elements leaving the edge, as well as button)
-$('.button_delete_horizontal').click(function () {
-		if(xDZCoord > 0) {
-				for(tempCount=0; tempCount <= yDZCoord; tempCount++) {
-					gridContract(xDZCoord,tempCount);
-			}
-			xDZCoord--
-			//Decrease array in the x direction
-			dzPosArr.length = xDZCoord+1;
-		}
+// //Button function for deleting dropzone columns (later this will be triggered by elements leaving the edge, as well as button)
+// $('.button_delete_horizontal').click(function () {
+// 		if(xDZCoord > 0) {
+// 				for(tempCount=0; tempCount <= yDZCoord; tempCount++) {
+// 					gridContract(xDZCoord,tempCount);
+// 			}
+// 			xDZCoord--
+// 			//Decrease array in the x direction
+// 			dzPosArr.length = xDZCoord+1;
+// 		}
 
-		sizeObjCont(xDZCoord,yDZCoord);
-});
+// 		sizeObjCont(xDZCoord,yDZCoord);
+// });
 
-//Button function for deleting dropzone rows (later this will be triggered by elements leaving the edge, as well as button)
-$('.button_delete_vertical').click(function () {
-		if(yDZCoord > 0) {
-				for(tempCount=0; tempCount <= xDZCoord; tempCount++) {
-					gridContract(tempCount,yDZCoord);
-			}
-		//Decrease array in the y direction
-		for (i=0; i < (xDZCoord+1); i++) {
-			dzPosArr[i].length--;
-		};
-			yDZCoord--
-		}
+// //Button function for deleting dropzone rows (later this will be triggered by elements leaving the edge, as well as button)
+// $('.button_delete_vertical').click(function () {
+// 		if(yDZCoord > 0) {
+// 				for(tempCount=0; tempCount <= xDZCoord; tempCount++) {
+// 					gridContract(tempCount,yDZCoord);
+// 			}
+// 		//Decrease array in the y direction
+// 		for (i=0; i < (xDZCoord+1); i++) {
+// 			dzPosArr[i].length--;
+// 		};
+// 			yDZCoord--
+// 		}
 
-		sizeObjCont(xDZCoord,yDZCoord);
-});
+// 		sizeObjCont(xDZCoord,yDZCoord);
+// });
 
-//TODO:  Store elements deleted so user can undo
-//TODO:  When we delete elements we need to modify array of elements for interactjs
-$('.delete_elements').click(function(){
-	var numElements = $('.selected').length;
+// //TODO:  Store elements deleted so user can undo
+// //TODO:  When we delete elements we need to modify array of elements for interactjs
+// $('.delete_elements').click(function(){
+// 	var numElements = $('.selected').length;
 
-	for(var i=0; i<numElements; i++){
-		elArr.pop();
-	}
+// 	for(var i=0; i<numElements; i++){
+// 		elArr.pop();
+// 	}
 
-	$('.selected').remove();
-});
+// 	$('.selected').remove();
+// });
 
-});
+// });
